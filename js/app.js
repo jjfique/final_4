1c69bfadbd938bca028d4f5e3233fce9cc55ef87
jonathan <jjfique@gmail.com>
2019-09-15 18:39:49 -0500

    captura de numeros
    escribe numeros en pantalla

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var nunero1;
 var numero2;
 var operacion;
 
-// creacion del teclado 
+// creacion del variables teclado y pantalla 
 var resultado = document.getElementById('display');
   var reset = document.getElementById('on');
   var suma = document.getElementById('mas');
@@ -21,3 +21,75 @@ var resultado = document.getElementById('display');
   var ocho = document.getElementById('8');
   var nueve = document.getElementById('9');
   var cero = document.getElementById('0');
+
+  // captura de valores del teclado en pantalla 
+  
+  uno.onclick = function(e){
+    resultado.textContent = resultado.textContent  + "1";
+}
+dos.onclick = function(e){
+    resultado.textContent = resultado.textContent  + "2";
+}
+tres.onclick = function(e){
+    resultado.textContent = resultado.textContent  + "3";
+}
+cuatro.onclick = function(e){
+    resultado.textContent = resultado.textContent  + "4";
+}
+cinco.onclick = function(e){
+    resultado.textContent = resultado.textContent  + "5";
+}
+seis.onclick = function(e){
+    resultado.textContent = resultado.textContent  + "6";
+}
+siete.onclick = function(e){
+    resultado.textContent = resultado.textContent  + "7";
+}
+ocho.onclick = function(e){
+    resultado.textContent = resultado.textContent  + "8";
+}
+nueve.onclick = function(e){
+    resultado.textContent = resultado.textContent  + "9";
+}
+cero.onclick = function(e){
+    resultado.textContent = resultado.textContent  + "0";
+}
+reset.onclick = function(e){
+    resetear();
+}
+suma.onclick = function(e){
+    operandoa = resultado.textContent;
+    operacion = "+";
+    limpiar();
+}
+resta.onclick = function(e){
+    operandoa = resultado.textContent;
+    operacion = "-";
+    limpiar();
+}
+multiplicacion.onclick = function(e){
+    operandoa = resultado.textContent;
+    operacion = "*";
+    limpiar();
+}
+division.onclick = function(e){
+    operandoa = resultado.textContent;
+    operacion = "/";
+    limpiar();
+}
+igual.onclick = function(e){
+    operandob = resultado.textContent;
+    resolver();
+}
+
+
+//  metodos para resetear y limpiar pantalla 
+function limpiar(){
+  resultado.textContent = "";
+}
+function resetear(){
+  resultado.textContent = "0";
+  operandoa = 0;
+  operandob = 0;
+  operacion = "";
+}
