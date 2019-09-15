66c116ec982cb0c6f180adfb14ca7425e55d47f0
jonathan <jjfique@gmail.com>
2019-09-15 18:27:55 -0500

    creacion de variables de teclado

---
+++ js/app.js
@@ -0,0 +1,23 @@
+// variables globales 
+var nunero1;
+var numero2;
+var operacion;
+
+// creacion del teclado 
+var resultado = document.getElementById('display');
+  var reset = document.getElementById('on');
+  var suma = document.getElementById('mas');
+  var resta = document.getElementById('menos');
+  var multiplicacion = document.getElementById('por');
+  var division = document.getElementById('dividido');
+  var igual = document.getElementById('igual');
+  var uno = document.getElementById('1');
+  var dos = document.getElementById('2');
+  var tres = document.getElementById('3');
+  var cuatro = document.getElementById('4');
+  var cinco = document.getElementById('5');
+  var seis = document.getElementById('6');
+  var siete = document.getElementById('7');
+  var ocho = document.getElementById('8');
+  var nueve = document.getElementById('9');
+  var cero = document.getElementById('0');
