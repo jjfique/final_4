c73a3ba934b718708b56bcfc92326a22af4f2819
jonathan <jjfique@gmail.com>
2019-09-16 22:45:58 -0500

    efectos de teclas
    se crea el efecto de oprimir las teclas

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
-// variables globales 
+// variables globales
 var nunero1;
 var numero2;
 var operacion;
 
-// creacion del variables teclado y pantalla 
+
+
+// creacion del variables teclado y pantalla
 var resultado = document.getElementById('display');
   var reset = document.getElementById('on');
   var suma = document.getElementById('mas');
@@ -22,22 +24,58 @@ var resultado = document.getElementById('display');
   var nueve = document.getElementById('9');
   var cero = document.getElementById('0');
 
-  // captura de valores del teclado en pantalla 
-  
+  // captura de valores del teclado en pantalla
+
+
   uno.onclick = function(e){
-    resultado.textContent = resultado.textContent  + "1";
+    if (resultado.textContent=="0") {
+      resultado.innerHTML= "" ;
+      resultado.textContent = resultado.textContent  + "1"     
+    }
+    else if (resultado.textContent>=0)  {
+      resultado.textContent = resultado.textContent  + "1";
+    }
 }
+
+
 dos.onclick = function(e){
+  if (resultado.textContent=="0") {
+    resultado.innerHTML= "" ;
     resultado.textContent = resultado.textContent  + "2";
+
+  }
+  else if (resultado.textContent>=0)  {
+    resultado.textContent = resultado.textContent  + "2";
+  }
+
 }
 tres.onclick = function(e){
+  if (resultado.textContent=="0") {
+    resultado.innerHTML= "" ;
+    resultado.textContent = resultado.textContent  + "3" ;   
+  }
+  else if (resultado.textContent>=0)  {
     resultado.textContent = resultado.textContent  + "3";
+  }
 }
+
 cuatro.onclick = function(e){
+  if (resultado.textContent=="0") {
+    resultado.innerHTML= "" ;
+    resultado.textContent = resultado.textContent  + "4" ;   
+  }
+  else if (resultado.textContent>=0)  {
     resultado.textContent = resultado.textContent  + "4";
+  }
 }
 cinco.onclick = function(e){
+  if (resultado.textContent=="0") {
+    resultado.innerHTML= "" ;
+    resultado.textContent = resultado.textContent  + "5" ;   
+  }
+  else if (resultado.textContent>=0)  {
     resultado.textContent = resultado.textContent  + "5";
+  }
 }
 seis.onclick = function(e){
     resultado.textContent = resultado.textContent  + "6";
@@ -81,7 +119,7 @@ igual.onclick = function(e){
     operandob = resultado.textContent;
     resolver();
 }
-//  metodos para resetear y limpiar pantalla 
+//  metodos para resetear y limpiar pantalla
 function limpiar(){
   resultado.textContent = "";
 }
@@ -92,7 +130,7 @@ function resetear(){
   operacion = "";
 }
 
-/// se realiza un selector de operacion 
+/// se realiza un selector de operacion
 function resolver(){
   var res = 0;
   switch(operacion){
@@ -113,3 +151,83 @@ function resolver(){
   resultado.textContent = res;
 }
 
+// Efecto de teclas
+
+  uno.addEventListener("mousedown", function () {
+  uno.style.transform = "scale(.95, .95)";
+  });
+  uno.addEventListener("mouseup", function () {
+  uno.setAttribute("style", "transform:scale(1, 1)");
+
+  });
+
+  dos.addEventListener("mousedown", function () {
+  dos.style.transform = "scale(.95, .95)";
+  });
+  dos.addEventListener("mouseup", function () {
+  dos.setAttribute("style", "transform:scale(1, 1)");
+
+  });
+
+  tres.addEventListener("mousedown", function () {
+  tres.style.transform = "scale(.95, .95)";
+  });
+  tres.addEventListener("mouseup", function () {
+  tres.setAttribute("style", "transform:scale(1, 1)");
+
+  });
+  cuatro.addEventListener("mousedown", function () {
+  cuatro.style.transform = "scale(.95, .95)";
+  });
+  cuatro.addEventListener("mouseup", function () {
+  cuatro.setAttribute("style", "transform:scale(1, 1)");
+
+  });
+  cinco.addEventListener("mousedown", function () {
+  cinco.style.transform = "scale(.95, .95)";
+  });
+  cinco.addEventListener("mouseup", function () {
+  cinco.setAttribute("style", "transform:scale(1, 1)");
+
+  });
+  seis.addEventListener("mousedown", function () {
+  seis.style.transform = "scale(.95, .95)";
+  });
+  seis.addEventListener("mouseup", function () {
+  seis.setAttribute("style", "transform:scale(1, 1)");
+
+  });
+  siete.addEventListener("mousedown", function () {
+  siete.style.transform = "scale(.95, .95)";
+  });
+  siete.addEventListener("mouseup", function () {
+  siete.setAttribute("style", "transform:scale(1, 1)");
+
+  });
+  ocho.addEventListener("mousedown", function () {
+  ocho.style.transform = "scale(.95, .95)";
+  });
+  ocho.addEventListener("mouseup", function () {
+  ocho.setAttribute("style", "transform:scale(1, 1)");
+
+  });
+  nueve.addEventListener("mousedown", function () {
+  nueve.style.transform = "scale(.95, .95)";
+  });
+  nueve.addEventListener("mouseup", function () {
+  nueve.setAttribute("style", "transform:scale(1, 1)");
+
+  });
+
+
+/*function trasformar() {
+  for ( let i = 0; i < botones.length; i++) {
+    botones[i].addEventListener("mousedown", function () {
+    botones[i].style.transform = "scale(.95, .95)";
+    });
+    botones[i].addEventListener("mouseup", function () {
+    botones[i].setAttribute("style", "transform:scale(1, 1)");
+
+    });
+    };
+  }*/
